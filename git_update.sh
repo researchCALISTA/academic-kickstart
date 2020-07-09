4cd84e280ba5e084ac4889037c81e409b501ab21
Lizardie <lizardie@gmail.com>
2020-07-09 22:43:31 +0200

    dvlp website Thu Jul  9 22:43:31 CEST 2020

--- git_update.sh
+++ git_update.sh
@@ -2,28 +2,28 @@
 cd /Users/lizardie/Dropbox/CODES/Hugo
 ls -lah
 cd calista
-git status
-git add .
-git commit -m "dvlp website `date`"
-
-git config --local credential.helper ""
-git push origin master
 
 
 
+git status ; git add .  ; git commit -m "dvlp website `date`" ; git push origin master
 
-# untracked problem
-git status
-#	modified:   calista (modified content, untracked content)
-git rm -rf --cached calista
-git add calista
-git rm -rf --cached themes/academic
-git submodule add --recursive /Users/lizardie/Dropbox/CODES/Hugo/calista/themes/academic
-git commit -m "remove cached repo but add all else"
-cd /Users/lizardie/Dropbox/CODES/Hugo/calista/themes/academic
-rm .git
-cd ..
-cd ..
-pwd
-/Users/lizardie/Dropbox/CODES/Hugo/calista
-git commit -m "add incl themes repo "
+
+#git config --local credential.helper ""
+
+
+#
+# # untracked problem
+# git status
+# #	modified:   calista (modified content, untracked content)
+# git rm -rf --cached calista
+# git add calista
+# git rm -rf --cached themes/academic
+# git submodule add --recursive /Users/lizardie/Dropbox/CODES/Hugo/calista/themes/academic
+# git commit -m "remove cached repo but add all else"
+# cd /Users/lizardie/Dropbox/CODES/Hugo/calista/themes/academic
+# rm .git
+# cd ..
+# cd ..
+# pwd
+# /Users/lizardie/Dropbox/CODES/Hugo/calista
+# git commit -m "add incl themes repo "
